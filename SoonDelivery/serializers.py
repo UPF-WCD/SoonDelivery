be20c6b4688002ac8bcb5acf33a44dce4f941dc6
tjdgus <tjdgus1977@gmail.com>
2021-10-28 04:26:29 +0900

    [BE] 이메일 전송 구현

--- SoonDelivery/serializers.py
+++ SoonDelivery/serializers.py
@@ -1,24 +1,50 @@
 from rest_framework import serializers
 from .models import Test, User,Check_list
 from django.contrib.auth import authenticate
+from .token import accout_activation_token
+from .text import message
+from django.core.validators import validate_email
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_bytes,force_text
 
 class TestSerializer(serializers.ModelSerializer):
   class Meta:
     model = Test
     fields = '__all__'
+
 #회원가입
 class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username", "password", "school_email")
+        fields = ("id", "username", "password", "school_email","is_active")
         extra_kwargs = {"password": {"write_only": True}}
+
+    def validate_email(self, value):
+        if User.objects.filter(school_email = value).exists():
+            raise serializers.ValidationError("이메일이 이미 존재합니다.")
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            validated_data["username"],  validated_data["password"], validated_data["school_email"]
+            validated_data["username"],  validated_data["password"], validated_data["school_email"],
+            is_active=False
         )
         user.set_password(validated_data['password'])
         user.save()
+        #여기서 현재 주소값을 넘겨줘야하는데 못 넘겨주고 있음
+        # current_site = get_current_site(get_host)
+        # domain = current_site.domain
+        # uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+        # token = accout_activation_token.make_token(user)
+        # message_data = message(domain, uidb64, token)
+
+        # mail_title = "이메일 인증을 완료해주세요"
+        # mail_to = validated_data["school_email"]
+        # email = EmailMessage(mail_title, message_data, to=[mail_to])
+        # email.send()
+
         return user
 
 #접속 유지 중인가?
--- SoonDelivery/views.py
+++ SoonDelivery/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import get_object_or_404, render
+from io import StringIO
+from json import decoder, encoder
+import jwt
+import json
+from django.shortcuts import get_object_or_404, render, redirect
 from rest_framework import serializers, viewsets, permissions, generics
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.utils.serializer_helpers import JSONBoundField
 from .serializers import (
     TestSerializer, 
     CreateUserSerializer,
@@ -10,8 +15,20 @@ from .serializers import (
     LoginUserSerializer,
     CheckListSerializer, 
     CheckSerializer)
-from .models import Test, Check_list
+from .models import Test, Check_list,User
 from knox.models import AuthToken
+
+from .token import accout_activation_token
+from .text import message
+
+from django.views import View
+from django.http import HttpResponse, JsonResponse
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_bytes,force_text
 
 
 class TestView(viewsets.ModelViewSet):
@@ -22,20 +39,67 @@ class RegistrationAPI(generics.GenericAPIView):
     serializer_class = CreateUserSerializer
 
     def post(self, request, *args, **kwargs):
-        if len(request.data["username"]) < 6 or len(request.data["password"]) < 4:
-            body = {"message": "short field"}
-            return Response(body, status=status.HTTP_400_BAD_REQUEST)
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        return Response(
-            {
+        # print(type(request.body))
+        # print(request.body)
+        # bod = request.body.decode('utf8').replace("'", '"')
+        # print(type(bod))
+        # print(bod)
+        # datas=json.loads(bod)
+        # try:
+        #     validate_email(datas["school_email"])
+        #     if User.objects.filter(school_email=datas["school_email"]).exists():
+        #         return JsonResponse({"message" : "EXIST_EMAIL"}, status=400)
+            serializer = self.get_serializer(data=request.data)
+            print(request.data)
+            serializer.is_valid(raise_exception=True)
+            user = serializer.save()
+
+            current_site = get_current_site(request.get_host())
+            domain = current_site.domain
+            uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+            token = accout_activation_token.make_token(user)
+            message_data = message(domain, uidb64, token)
+
+            mail_title = "이메일 인증을 완료해주세요"
+            qd = request.data
+            emails = qd['school_email']
+            mail_to = emails
+            email = EmailMessage(mail_title, message_data, to=[mail_to])
+            email.send()
+
+            return Response(
+                {
                 "user": UserSerializer(
                     user, context=self.get_serializer_context()
                 ).data, 
+                #여기서 값을 넘겨주려고 했으나 쉽지 않음
                 "token": AuthToken.objects.create(user)[1],
-            }
-        )
+                }
+            )
+        # except KeyError:
+        #     return JsonResponse({"message" : "INVALID_KEY"}, status=200)
+        # except TypeError:
+        #     return JsonResponse({"message" : "INVALID_TYPE"}, status=400)
+        # except ValidationError:
+        #     return JsonResponse({"message" : "VALIDATION_ERROR"}, status=400)
+
+class Activate(View):
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64.encode('utf-8')))
+            user = User.objects.get(pk = uid)
+
+            if accout_activation_token.check_token(user, token):
+                user.is_active = True
+                user.save()
+                return redirect('http://google.com')
+            return Response(user.school_email + ' 계정이 활성화되었습니다.',status=status.HTTP_200_OK)
+        except ValidationError:
+            return JsonResponse({"message":"TYPE_ERROR"}, status=400)
+        except KeyError:
+            return JsonResponse({"message":"INVALID_KEY"}, status=400)
+
+
 class LoginAPI(generics.GenericAPIView):
     serializer_class = LoginUserSerializer
 
